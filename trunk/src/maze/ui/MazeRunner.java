6bc2eb8154d98e2cd11b6046188b50dea3a2ed4d
benwarfield <benwarfield@9df4b050-cb20-11de-b822-9751620e1d2d>
2009-11-07 07:09:43 +0000

    Made MazeRunner capable of running a tournament round of some kind.
    Arguably it should have timeout capability, but that's got SEP written all over it.

--- trunk/src/maze/ui/MazeRunner.java
+++ trunk/src/maze/ui/MazeRunner.java
@@ -3,47 +3,87 @@
  */
 package maze.ui;
 
+import java.util.ArrayList;
+
 /**
  * @author ben
  *
  */
 public class MazeRunner {
-
+	enum runType {RUN_ONCE, RUN_MULTIPLE, RUN_TOURNEY};
+	static final String PACKAGE_PREFIX = "maze.";
 	/**
+	 * Argument forms allowed:
+	 * Player mazename object_count [repeats]
+	 * mazename object_count repeats [player-name-filter]
+	 *
+	 * In the first case, the given player will be run through the given maze with the given number of objects, and the score printed.
+	 * If the optional "repeats" argument is supplied, it will be run that number of times, and the scores listed on a single line.
+	 * In the second form, all players will be run with the given object count and repetitions.  If the optional
+	 * filter argument is supplied, then only players whose names start with that string (e.g. "maze.g4") will be evaluated.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		IOController iocontrol = new IOController();
-		String playerName = args[0];
-		String mazeName = args[1];
-		int object_count = Integer.parseInt(args[2]);
-		if (3 == args.length) {
-			String msg = String.format(
+		String playerName = null;
+		String mazeName = null;
+		int object_count = -1;
+		int rep_count = 1;
+		runType type = runType.RUN_ONCE;
+		String msg;
+
+		if (args[0].startsWith(PACKAGE_PREFIX)) {
+			playerName = args[0];
+			mazeName = args[1];
+			object_count = Integer.parseInt(args[2]);
+			msg = String.format(
 					"Running maze %s with player %s and %d objects",
 					new Object[]{mazeName,playerName, object_count}
 			);
-			System.out.println(msg);
-			int score = runMaze(iocontrol, playerName, mazeName, object_count);
-			System.out.println("Finished: " + score);
-		} else {
-			int rep_count = Integer.parseInt(args[3]);
-			int scores[] = new int[rep_count];
-			String msg = String.format(
+			if (4 == args.length) {
+				rep_count = Integer.parseInt(args[3]);
+
+				msg = String.format(
 					"Repeating maze %s with player %s and %d objects, %d times",
 					new Object[]{mazeName,playerName, object_count, rep_count}
-			);
+				);
+			}
 			System.out.println(msg);
-			for (int i = 0; i < rep_count; i++) {
-				scores[i] = runMaze(iocontrol, playerName, mazeName, object_count);
+			runRepeats(iocontrol, playerName, mazeName, object_count, rep_count);
+		} else {
+			mazeName = args[0];
+			object_count = Integer.parseInt(args[1]);
+			rep_count = Integer.parseInt(args[2]);
+			String prefix = 4 == args.length ? args[3] : PACKAGE_PREFIX;
+			ArrayList<String> allPlayers = iocontrol.getPlayerList();
+			for (String p : allPlayers) {
+				if (p.startsWith(prefix)) {
+					runRepeats(iocontrol, p, mazeName, object_count, rep_count);
+				}
 			}
-			for (int s : scores) {
-				System.out.print(Integer.toString(s));
-				System.out.print(' ');
-			}
-			System.out.print('\n');
-
 		}
 
+
+	}
+
+	/**
+	 * @param iocontrol
+	 * @param playerName
+	 * @param mazeName
+	 * @param object_count
+	 * @param rep_count
+	 */
+	private static void runRepeats(IOController iocontrol, String playerName, String mazeName, int object_count, int rep_count) {
+		int scores[] = new int[rep_count];
+		for (int i = 0; i < rep_count; i++) {
+			scores[i] = runMaze(iocontrol, playerName, mazeName, object_count);
+		}
+		System.out.print(playerName);
+		for (int s : scores) {
+			System.out.print('\t');
+			System.out.print(Integer.toString(s));
+		}
+		System.out.print('\n');
 	}
 
 	/**
